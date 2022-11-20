b00265a1302129e7482086dfe388a6ed197e42f0
Paresh Adhia <padhia@gmail.com>
2022-11-20 10:30:44 -0500

    Added build.nix

---
+++ build.nix
@@ -0,0 +1,18 @@
+{ lib, python }:
+python.pkgs.buildPythonPackage rec {
+  pname = "sfconn";
+  version = "0.2.1";
+  src = ./.;
+
+  propagatedBuildInputs = with python.pkgs; [
+    snowflake-connector-python
+  ];
+
+  doCheck = false;
+
+  meta = with lib; {
+    homepage = "https://github.com/padhia/sfconn";
+    description = "Snowflake connection helper functions";
+    maintainers = with maintainers; [ padhia ];
+  };
+}
